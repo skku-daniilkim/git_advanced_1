092f3b32443b76e5b4176dfed38c3e49c321755f
skku-daniilkim(Dan) <129740667+skku-daniilkim@users.noreply.github.com>
2024-04-18 10:06:28 +0000

    implemented even_list function

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def even_list(int_list: List[int]) -> List[int]:
     A list of even integers.
     """
     # TODO: Implement even_list
-    pass
+    return [n for n in int_list if n % 2 == 0 and n != 0]
+
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
@@ -26,7 +27,7 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
 # Main function
 def main():
     # Example list
-    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
     even_int_list = even_list(int_list)
     output = sum_of_squares_of_even(even_int_list)
     print(output)
